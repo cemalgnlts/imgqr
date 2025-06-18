ec2268b85d03bbc4709004ef073a4f012b394163
Cemal Gönültaş <cemalgnlts@gmail.com>
2025-06-18 14:03:24 +0300

    Add file drop support.

--- main.js
+++ main.js
@@ -1,15 +1,53 @@
 import { uploadFile, generateQRCode, getFileURL } from "./utils.js";
 
+/** @type {HTMLDivElement} */
 const dropArea = document.querySelector(".drop-area");
+/** @type {HTMLInputElement} */
 const fileInput = document.querySelector(".file-inp");
+/** @type {HTMLDivElement} */
 const qrContainer = document.querySelector(".qr-result");
 
-dropArea.onclick = () => fileInput.click();
 
 checkFile();
 
+dropArea.onclick = () => fileInput.click();
+
+dropArea.ondrop = (ev) => {
+    ev.preventDefault();
+    dropArea.classList.remove("drag-over");
+
+    let file;
+
+    if (ev.dataTransfer.items) {
+        // Use DataTransferItemList interface to access the file(s)
+        const [item] = ev.dataTransfer.items;
+        if (item.kind === "file") file = item.getAsFile();
+    } else {
+        // Use DataTransfer interface to access the file(s)
+        file = ev.dataTransfer.files[0];
+    }
+
+    if (file) startFileUpload(file);
+};
+
+dropArea.ondragover = (ev) => {
+    ev.preventDefault();
+    dropArea.classList.add("drag-over");
+};
+
+dropArea.ondragleave = dropArea.ondragend = (ev) => {
+    ev.preventDefault();
+    dropArea.classList.remove("drag-over");
+};
+
 fileInput.onchange = async () => {
     const [file] = fileInput.files;
+
+    await startFileUpload(file);
+}
+
+/** @param {File} file  */
+async function startFileUpload(file) {
     let code;
 
     dropArea.classList.add("loading");
